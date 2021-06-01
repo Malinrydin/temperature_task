b6ffcf99c9896a3c1db885cd850e6378f5104ccd
Malin <Malin-br@live.no>
2021-06-01 20:47:19 +0200

    La til getTemperature metoden og logikken for dette. Average blir hentet fra en annen metode.

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 /*
-double getTemperature(); skal returnere temperatur fra en temperaturmåler
 
-
-Lese temp fra en fil
-Konvertere temperaturen på filen til celsius
-    - temperaturen varierer fra -50 til +50 grader
-    - 2048 er ca 0C
-    - 3000 er ca 23C
-
-Skal sende det til HTTP REST endpoint
+Skal sende info til HTTP REST endpoint
     - getTemperature skal lese fra filen, men bare en linje hver 100ms
     - Applikasjonen skal kunne kalkulerere min, max, og average temperatur på perioder på 2 min
     - Hvert 2 minutt skal applikasjonen sende de lagrede verdiene til HTTP REST API:
@@ -28,44 +21,82 @@ Skal sende det til HTTP REST endpoint
             "max": number, // Maximum observed temperature
             "average": number // Average temperature
         }
-
-
-
  */
 public class Main {
     public static void main(String[] args) {
 
-
         File temperatures = new File("temperature.txt");
-        ArrayList<Integer> values = readFile(new File(String.valueOf(temperatures)));
-
-        System.out.println(values);
-
+        double values = getTemperature(new File(String.valueOf(temperatures)));
 
     }
 
-        public static ArrayList<Integer> readFile(File fileName) {
-            // Metoden leser innholdet i filen linje for linje
-            // Thread.sleep(100); gjør at programmet venter 100 ms før det kjører videre
-            // metoden returnerer hele txt filen lest i en String ArrayList
-            ArrayList<Integer> valueList = null;
-            try {
-                FileReader readTemp = new FileReader(fileName);
-                Scanner line = new Scanner(readTemp);
-                valueList = new ArrayList<Integer>();
+    public static double getTemperature(File fileName) {
+        // Metoden leser innholdet i filen linje for linje
+        // Thread.sleep(100); gjør at programmet venter 100 ms før det kjører videre
 
-                // Mens det finnes en linje, og den inneholder noe, så skal linjen bli lagt til i en arraylist
-                while (line.hasNextInt()) {
-                    valueList.add(line.nextInt());
+        double valueRead = 0;
+        double bitRes = 4096;
+        double Vref = 3300;
+        double stepsize = Vref/bitRes;
+        double stegMinMax = 100;
+        double mVprsteg = Vref / stegMinMax;
+        double voltIn = 0;
+        double temp = 0;
+        int count = 0;
+        ArrayList<Double> values = new ArrayList<Double>();
+
+        try {
+            FileReader readTemp = new FileReader(fileName);
+            Scanner line = new Scanner(readTemp);
+
+            // Mens det finnes en linje, og den inneholder noe, så skal linjen bli konvertert
+            while (line.hasNextDouble() && line.hasNext() ) {
+                // legger veriden i linjen i en variabel
+                valueRead = line.nextDouble();
+                // Finner LSB i stepsize (3300 / 4096), ganger denne med verdien som er lest for å finne
+                // volten tilhørende verdien.
+                voltIn = valueRead * (stepsize);
+                // Finner temperaturen ved å bruke voltIn og dele denne på Vref/stegMinMax
+                temp = (voltIn/ mVprsteg)-50;
+
+                // System.out.println(temp);
+                Thread.sleep(100);
+                count++;
+                values.add(temp);
+                //System.out.println(values);
+
+                if (count == 1200){
+                    double min = Collections.min(values);
+                    double max = Collections.max(values);
+
+                    System.out.println(
+                            "----------" +
+                            "\nMinimumverdi: " + min +
+                            "\nMaksimumverdi: " + max +
+                            "\nGjennomsnitt: " + average(values) +
+                            "\n----------");
+                    count = 0;
                 }
-
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
             }
-            return valueList;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-}
+        return valueRead;
+    }
 
+    public static double average(ArrayList<Double> getTempValues) {
+        double total = 0;
+        if (!getTempValues.isEmpty()) {
+            for (double i : getTempValues) {
+                total += i;
+            }
+            return total / getTempValues.size();
+        }
+        return total;
+    }
+}
 
 
 
