5b39dd7cf55d58f063335b4d60e915f6c1619e09
Malin <Malin-br@live.no>
2021-06-04 12:17:25 +0200

    Sett på Http POST req

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,52 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.*;
 
-/*
 
-Skal sende info til HTTP REST endpoint
-    - getTemperature skal lese fra filen, men bare en linje hver 100ms
-    - Applikasjonen skal kunne kalkulerere min, max, og average temperatur på perioder på 2 min
-    - Hvert 2 minutt skal applikasjonen sende de lagrede verdiene til HTTP REST API:
-        http://localhost:5000/api/temperature
-    - Verdiene skal bli sendt i JSON format med følgende format
-            // TemperatureMeasurement
-        {
-            "time": {
-                "start": string, // Start date and time in ISO8601 format for the measurement
-                "end": string // End date and time in ISO8601 format for the measurement
-            },
-            "min": number, // Minimum observed temperature
-            "max": number, // Maximum observed temperature
-            "average": number // Average temperature
-        }
- */
 public class Main {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws IOException {
+
+    /*
+        // https://www.baeldung.com/httpurlconnection-post
+
+            // URL
+        URL url = new URL ("http://localhost:5000/api/temperature");
+            // Åpner en connection
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            //  Sier at requesten er av typen POST
+        connection.setRequestMethod("POST");
+            // Sier at det er json som skal postes
+        connection.setRequestProperty("Content-Type", "application/json; utf-8");
+            // Sier at det skal aksepteres json
+        connection.setRequestProperty("Accept", "application/json");
+            // Gjør at man kan poste
+        connection.setDoOutput(true);
+
+        String jsonInputString = "{\"name\": \"Upendra\", \"job\": \"Programmer\"}";
+        try(OutputStream os = connection.getOutputStream()) {
+            byte[] input = jsonInputString.getBytes("utf-8");
+            os.write(input, 0, input.length);
+        }
+        try(BufferedReader br = new BufferedReader(
+                new InputStreamReader(connection.getInputStream(), "utf-8"))) {
+            StringBuilder response = new StringBuilder();
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) {
+                response.append(responseLine.trim());
+            }
+            System.out.println(response.toString());
+        }
+    */
+            // Lagrer tekst-filen i en variabel
         File temperatures = new File("temperature.txt");
-        double values = getTemperature(new File(String.valueOf(temperatures)));
+            // Kjører getTemperature metoden med filen som argument
+        getTemperature(temperatures);
 
     }
 
     public static double getTemperature(File fileName) {
-        // Metoden leser innholdet i filen linje for linje
-        // Thread.sleep(100); gjør at programmet venter 100 ms før det kjører videre
-
         double valueRead = 0;
         double bitRes = 4096;
         double Vref = 3300;
@@ -46,29 +59,32 @@ public class Main {
         ArrayList<Double> values = new ArrayList<Double>();
 
         try {
+                // Leser innholdet i filen linje for linje
             FileReader readTemp = new FileReader(fileName);
             Scanner line = new Scanner(readTemp);
 
-            // Mens det finnes en linje, og den inneholder noe, så skal linjen bli konvertert
+                // Mens det finnes en linje, og den inneholder noe, så skal linjen
+                // bli konvertert til riktig temperatur
             while (line.hasNextDouble() && line.hasNext() ) {
-                // legger veriden i linjen i en variabel
+                    // legger verdien i linjen i en variabel
                 valueRead = line.nextDouble();
-                // Finner LSB i stepsize (3300 / 4096), ganger denne med verdien som er lest for å finne
-                // volten tilhørende verdien.
+                    // Finner LSB i stepsize (3300 / 4096), ganger denne med verdien som er lest for å finne
+                    // volten tilhørende verdien.
                 voltIn = valueRead * (stepsize);
-                // Finner temperaturen ved å bruke voltIn og dele denne på Vref/stegMinMax
-                temp = (voltIn/ mVprsteg)-50;
+                    // Finner temperaturen ved å bruke voltIn og dele denne på Vref/stegMinMax
+                temp = (voltIn / mVprsteg) - 50;
 
-                // System.out.println(temp);
                 Thread.sleep(100);
+                    // la til en count. Når denne er 1200 har det gått to minutter.
+                    // Burde kanskje heller bruke en slags timer?
                 count++;
                 values.add(temp);
                 //System.out.println(values);
 
-                if (count == 1200){
+                // 1200 er to minutter, bruker 100 under testing for å sjekke at det stemmer. 100 er 10 sek.
+                if (count == 100){
                     double min = Collections.min(values);
                     double max = Collections.max(values);
-
                     System.out.println(
                             "----------" +
                             "\nMinimumverdi: " + min +
@@ -76,6 +92,7 @@ public class Main {
                             "\nGjennomsnitt: " + average(values) +
                             "\n----------");
                     count = 0;
+                    values.clear();
                 }
             }
         } catch (FileNotFoundException e) {
